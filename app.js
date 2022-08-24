544405be2d1b9a66d1094285c50e6d86a16cf4bb
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-08-24 23:02:17 +0900

    painting Line

--- app.js
+++ app.js
@@ -2,10 +2,34 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = 2;
 
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-ctx.lineTo(150, 150);
-ctx.lineTo(50, 150);
-ctx.lineTo(50, 50);
-ctx.fill();
+const colors = [
+  "red",
+  "blue",
+  "green",
+  "yellow",
+  "orange",
+  "purple",
+  "pink",
+  "black",
+  "brown",
+  "grey",
+  "white",
+  "cyan",
+  "magenta",
+  "lime",
+  "gold",
+  "silver",
+];
+
+function onClick(event) {
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
+
+canvas.addEventListener("mousemove", onClick);
